342a11e9f3d5c9c0a8bdd59fc7e033120069d919
AyushChaubey819 <i.ayushchaubey@gmail.com>
2026-07-24 10:25:22 +0530

    Passing by parameters

---
+++ prg1.cpp
@@ -0,0 +1,22 @@
+// ye toh ho gya ek simple sa OOPS ka example but in the static form and also  Not given by  user input  
+#include<bits/stdc++.h>
+using namespace std ;
+
+class Student{
+    public:
+    int marks;
+    string name;
+    float percent;
+
+    Student(string n,int m, float per){
+         name = n;
+         marks = m ;
+         percent = per;
+    }
+
+};
+int main(){
+   
+    
+   Student s1 = new Student("Rohan" ,);
+}
