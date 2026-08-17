789a64689ad1b1efaeb252a42f13ab2d37ba3d37
AyushChaubey819 <i.ayushchaubey@gmail.com>
2026-08-17 12:05:57 +0530

    Auto and Range  based loops  Examples

---
+++ All Experiments/Experiment-04.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> num = {10, 20, 30, 40, 50};
+    cout << "Elements of the collection:\n";
+
+    for (auto value : num){
+        cout<<value<< " ";
+    }
+
+    return 0;
+}
