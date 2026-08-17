7a597ebe63de0d987cf8be48a1244d21c5842556
AyushChaubey819 <i.ayushchaubey@gmail.com>
2026-08-17 12:08:10 +0530

    Changes

--- BankAccountusingOOPS.cpp
+++ BankAccountusingOOPS.cpp
@@ -12,7 +12,48 @@ public :
     }
 
     void deposit(int rs){
-        
+        #include <bits/stdc++.h>
+using namespace std;
+
+class Account {
+private:
+    int amt;
+    string name;
+
+public:
+    
+    void setAmount(int balance) {
+        amt = balance;
+    }
+
+    void withdraw(int rs) {
+        if (rs <= amt) {
+            amt -= rs;
+            cout << "Balance after withdrawal: " << amt << endl;
+        } else {
+            cout << "Insufficient balance!" << endl;
+        }
+    }
+
+    void deposit(int rs) {
+        amt += rs;
+        cout << "Balance after deposit: " << amt << endl;
+    }
+} ;
+
+int main() {
+    int amt;
+    cout << "Enter the Initial amount: ";
+    cin >> amt;
+
+    Account A1;
+    A1.setAmount(amt);
+
+    A1.deposit(25000);
+    A1.withdraw(10000);
+
+    return 0;
+}
         amt+=rs ;
         cout<<amt <<endl;
     }
