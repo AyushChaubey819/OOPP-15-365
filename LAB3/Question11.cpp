6e991539b07b729a6ea56b211f920d1433d25807
AyushChaubey819 <i.ayushchaubey@gmail.com>
2026-09-25 20:29:42 +0530

    Question 11 is done

--- LAB3/Question11.cpp
+++ LAB3/Question11.cpp
@@ -1,40 +1,21 @@
 #include<iostream>
 using namespace std;
 
-inline double area(double length,double width=0){
-    return length*width ;
-}
-inline double area(float radius){
-    return 3.14000*radius*radius  ;
+inline int add(int a=0,int b=0){
+    return a+b ;
+
 }
 
-inline int  area(int size=0){
-    return size*size;
+inline int add(int a,int b,int c){
+    return a+b+c ;
 }
-
-
-
+float add(float a,float b){
+    return a+b; 
+}
 int main(){
-
-    double l,b;
-    cout<<"Enter the length: ";
-    cin>>l;
-
-    cout<<"Enter the width: ";
-    cin>>b ;
-
-    double radius ;
-    int size ;
-    cout<<"Enter the radius : ";
-    cin>>radius;
-
-    cout<<"Enter the size: ";
-    cin>>size; 
-
-
-    cout<<"Area of rectangle : "<<area(l,b)<<endl;
-    cout<<"Area of Circle : "<< area(radius)<<endl;
-    cout<<"Area of Square : "<< area(size)<<endl;
-    
-
+ 
+    int a,b;
+    cout<<"Enter the numbers: ";
+    cin>>a>>b ;
+return 0;
 }
